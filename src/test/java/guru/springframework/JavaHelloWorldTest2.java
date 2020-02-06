e0748d9a54701f89199cc7ef1adff124ba86baff
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-02-05 22:56:52 -0300

    Using Maven Surefire for JUnit 4 Unit Tests

---
+++ src/test/java/guru/springframework/JavaHelloWorldTest2.java
@@ -0,0 +1,16 @@
+package guru.springframework;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JavaHelloWorldTest2 {
+
+	@Test
+	public void testGetHello() {
+		JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+
+		assertEquals("Hello World", javaHelloWorld.getHello());
+	}
+
+}
